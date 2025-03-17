77af36650e1410b04e6611a9f35b3ab81af35f01
ashraf <sadarsign@gmail.com>
2025-03-17 22:15:42 +0300

    Initialized

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector(".guess").value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const gues = Number(document.querySelector('.guess').value);
@@ -27,8 +28,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //when guess is too high
   } else if (gues > secretNumber) {
